9d3b07d7a6b514f6698feed3e3ab9040aca2befe
Enzo Peigné <enzo.peigne@isen-ouest.yncrea.fr>
2023-05-15 11:56:33 +0200

    All users created

--- main.sh
+++ main.sh
@@ -10,11 +10,17 @@ awk -F ';' 'NR>1 { print $2}' "$csv_file">surname.txt
 awk -F ';' 'NR>1 { print $3}' "$csv_file">mail.txt
 awk -F ';' 'NR>1 { print $4}' "$csv_file">password.txt
 
-#creation variables data user (1ere personne pour l'instant)
-name=$(awk 'NR==1{print $1}' name.txt)
-surname=$(awk 'NR==1{print $1}' surname.txt)
-mail=$(awk 'NR==1{print $1}' mail.txt)
-passwd=$(awk 'NR==1{print $1}' password.txt)
+#avoir le nombre de lignes du csv
+nb_lignes=$(wc -l < $csv_file)
+
+#creation variables data user
+for((i=1;i<=$nb_lignes;i++));
+do
+
+name=$(awk -v line="$i" 'NR==line{print $1}' name.txt)
+surname=$(awk -v line="$i" 'NR==line{print $1}' surname.txt)
+mail=$(awk -v line="$i" 'NR==line{print $1}' mail.txt)
+passwd=$(awk -v line="$i" 'NR==line{print $1}' password.txt)
 
 #print de verif
 #echo $name $surname $mail $passwd
@@ -24,17 +30,26 @@ first_letter=${name:0:1}
 username=$(echo $first_letter$surname | tr '[:upper:]' '[:lower:]')
 
 #creation user avec adduser et set passwd
-sudo useradd $username
+sudo useradd -s /bin/bash -m $username
 echo $username:$passwd | sudo chpasswd
 
 #expiration du mot de passe
 sudo chage --lastday 0 $username
 
-#creation du home
-sudo mkdir /home/$username
-sudo chown $username:$username /home/$username
+#creation dossier sauvegarde 
+sudo mkdir /home/$username/a_sauver
 
+#creation dossier shared
+if [ ! -d /home/shared ]
+then
+sudo mkdir /home/shared
+sudo chown root:root /home/shared
+sudo chmod 775 /home/shared
+fi
 
+#creation dossier user dans shared
+sudo mkdir /home/shared/$username
+sudo chown $username:$username /home/shared/$username
+sudo chmod 755 /home/shared/$username
 
-
-
+done
